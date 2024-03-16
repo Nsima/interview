6b2980939e536783ad888cb2c640596489000b03
Nsima George <georgensima@gmail.com>
2024-03-16 17:17:30 +0100

    Update hardhat.config.js
    Added the Alchemy Key

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -2,6 +2,22 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
 module.exports = {
+  networks: {
+    // For Alchemy, you typically use the `rinkeby`, `ropsten`, `kovan`, or `mainnet` network
+    // Adjust `url` and `accounts` properties accordingly
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    }, 
+    ropsten: {
+    //   url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
+    // mainnet: {
+    //   url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
+  },
   solidity: {
     version: "0.8.24",
     settings: {
